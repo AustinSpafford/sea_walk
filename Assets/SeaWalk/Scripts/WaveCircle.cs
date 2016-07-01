4f8446ada09f31f57a83ef3fc2cf7f646fe6658b
Austin Spafford <austin.spafford@gmail.com>
2016-07-01 03:15:34 -0700

    First functional wave-circle
    It's currently a little bit bugged, but here's the first
    visible/explorable wave-circle! :)

---
+++ Assets/SeaWalk/Scripts/WaveCircle.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveCircle : MonoBehaviour
+{
+	public GameObject SingleWavePrefab = null;
+
+	public int WaveCount = 4;
+
+	public void Update()
+	{
+		if ((waveInstances == null) ||
+			(waveInstances.Length != WaveCount))
+		{
+			if (waveInstances != null)
+			{
+				foreach (GameObject waveInstance in waveInstances)
+				{
+					Destroy(waveInstance);
+				}
+
+				waveInstances = null;
+			}
+
+			waveInstances = new GameObject[WaveCount];
+
+			for (int waveIndex = 0;
+				waveIndex < waveInstances.Length;
+				++waveIndex)
+			{
+				float waveSeriesFraction = (waveIndex / (float)waveInstances.Length);
+
+				Quaternion waveOrientation = 
+					Quaternion.AngleAxis(
+						(waveSeriesFraction * 180),
+						Vector3.forward);
+
+				GameObject waveInstance = 
+					GameObject.Instantiate(SingleWavePrefab) as GameObject;
+
+				waveInstance.transform.parent = transform;
+				waveInstance.transform.localPosition = Vector3.zero;
+				waveInstance.transform.localRotation = waveOrientation;
+				waveInstance.transform.localScale = Vector3.one;
+
+				// Offset the wave's animation.
+				{
+					WaveyLineAnimator waveAnimator = 
+						waveInstance.GetComponent<WaveyLineAnimator>();
+
+					waveAnimator.CurrentAnimationFraction = waveSeriesFraction;
+				}
+
+				waveInstances[waveIndex] = waveInstance;
+			}
+		}
+	}
+
+	public GameObject[] waveInstances = null;
+}
---
+++ Assets/SeaWalk/Scripts/WaveyLineAnimator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class WaveyLineAnimator : MonoBehaviour
+{
+	public float WavePeriod = 3.0f;
+	public float WaveAmplitude = 0.5f;
+	public float WaveWavelength = 0.25f;
+
+	[Range(1, 1000)]
+	public int WaveSegmentCount = 30;
+
+	public float CurrentAnimationFraction = 0.0f;
+
+	public bool DebugEnabled = false;
+
+	public void Awake()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+	}
+
+	public void Update()
+	{
+		// Advance the animation.
+		if (Mathf.Approximately(WavePeriod, 0.0f) == false)
+		{
+			CurrentAnimationFraction = 
+				Mathf.Repeat(
+					(CurrentAnimationFraction + (Time.deltaTime / WavePeriod)),
+					1.0f);
+		}
+		
+		// Update the line renderer to accept our new positions.
+		{
+			int neededPositionCount = (WaveSegmentCount + 1);
+
+			if ((scratchLinePositions == null) ||
+				(scratchLinePositions.Length != neededPositionCount))
+			{
+				scratchLinePositions = new Vector3[neededPositionCount];
+
+				lineRenderer.SetVertexCount(scratchLinePositions.Length);
+			}
+		}
+
+		// Generate new positions and push them to the line renderer.
+		{
+			for (int positionIndex = 0;
+				positionIndex < scratchLinePositions.Length;
+				++positionIndex)
+			{
+				float positionFraction = (positionIndex / (float)(scratchLinePositions.Length - 1));
+
+				float waveSampleFraction = (
+					(positionFraction / WaveWavelength) +
+					CurrentAnimationFraction);
+
+				scratchLinePositions[positionIndex] = 
+					new Vector3(
+						0.0f,
+						(WaveAmplitude * Mathf.Sin((2 * Mathf.PI) * waveSampleFraction)),
+						positionFraction);
+			}
+
+			lineRenderer.SetPositions(scratchLinePositions);
+		}
+	}
+	
+	private LineRenderer lineRenderer = null;
+
+	private Vector3[] scratchLinePositions = null;
+}
